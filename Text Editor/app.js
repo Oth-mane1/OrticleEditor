fd23c4e3fa449fb8ebc0d2a847aa8ce8ce3d4b4d
Othmane Ouirdy <55802592+Oth-mane1@users.noreply.github.com>
2021-03-09 14:39:19 +0100

    Handle the paste event
    Handle the paste event the prevent users from copying html content with its style

--- Text Editor/app.js
+++ Text Editor/app.js
@@ -116,3 +116,15 @@ mode.onclick = function () {
         });
     }
 };
+
+// Handle the paste event to prevent user to paste a text with style
+document.getElementsByClassName("editor")[0].addEventListener("paste", function(e) {
+    // cancel paste
+    e.preventDefault();
+
+    // get text representation of clipboard
+    var text = (e.originalEvent || e).clipboardData.getData('text/plain');
+
+    // insert text manually
+    document.execCommand("insertHTML", false, text);
+});
