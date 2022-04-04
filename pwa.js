a73448bfa2b3920d5e7e97f7af5fb16ce284174c
Oth-mane1 <othmane.ouirdy@gmail.com>
2022-04-04 18:48:52 +0000

    Fix the path os sw

--- pwa.js
+++ pwa.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/service-worker.js")
+    navigator.serviceWorker.register("./service-worker.js")
         .then(serviceWorker => {
             console.log("Service Worker registered: ", serviceWorker);
         })
